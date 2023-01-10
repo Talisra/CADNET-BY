969a2e635d4972ec8f68cbab34cfd7318461cc89
BobYa1 <borisyazm@gmail.com>
2023-01-10 20:53:36 +0200

    Working database generation with balanced weights First commit for GT76 branch

--- LFD_CNN.py
+++ LFD_CNN.py
@@ -20,7 +20,7 @@ def network():
     test_folder = 'Data_lfd_split_train_test/test_dir'
     target_size = (256, 256)
     batch_size = 20
-    np_epochs = 1 #100
+    np_epochs = 2 #100
     dropout = 0.2
 
     train_datagen = ImageDataGenerator(rescale=1./ 255,
